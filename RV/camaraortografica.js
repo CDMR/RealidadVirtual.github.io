cb32cff8aa435c06cd75370ec7592dd8fb54c472
CarlosdMR <carlosd.marceleno@gmail.com>
2017-03-07 14:18:16 -0600

    Update camaraortografica.js

--- RV/camaraortografica.js
+++ RV/camaraortografica.js
@@ -7,4 +7,21 @@ camara.near=0.1;
 camara.far=1000;
 camara.updateProjectionMatrix();
 
-camara.position.z=100();
+camara.position.z=100;
+
+var cubo = new THREE.Mesh(new THREE.BoxGeometry(30,30,30),new THREE.MeshNormalMaterial());
+cubo.rotateY(Math.PI/4);
+var esfera1 = new THREE.Mesh(new THREE.SphereGeometry(15),new THREE.MeshNormalMaterial());
+esfera 1
+
+
+
+
+var escena = new THREE.Scene();
+ escena.add(esfera1);
+ escena.add(esfera2);
+ escena.add(cubo);
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
