ae97abe5b746a106e6310118843133a0cd4a85dd
CarlosdMR <carlosd.marceleno@gmail.com>
2017-03-08 03:03:16 -0600

    Update camaraortografica.js

--- RV/camaraortografica.js
+++ RV/camaraortografica.js
@@ -11,11 +11,12 @@ camara.position.z=100;
 
 var cubo = new THREE.Mesh(new THREE.BoxGeometry(30,30,30),new THREE.MeshNormalMaterial());
 cubo.rotateY(Math.PI/4);
-var esfera1 = new THREE.Mesh(new THREE.SphereGeometry(15),new THREE.MeshNormalMaterial());
-esfera 1
 
-
-
+var esfera1= new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
+esfera1.position.x=5;
+var esfera2= new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
+esfera2.position.x=-5;
+esfera2.position.z=-5;
 
 var escena = new THREE.Scene();
  escena.add(esfera1);
