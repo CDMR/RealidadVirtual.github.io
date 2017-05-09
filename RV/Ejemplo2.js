9da053562b9f7d90f3ec9dc69b74d8fb2b8d7c3e
CarlosdMR <carlosd.marceleno@gmail.com>
2017-05-09 13:51:03 -0500

    Create Ejemplo2.js

---
+++ RV/Ejemplo2.js
@@ -0,0 +1,28 @@
+function setup() {
+var Textura = THREE.ImageUtils.loadTexture('http://')
+var material = new THREE.MeshLambertMaterial({map: textura});
+var forma =new THREE.BoxGeometry(1,4,9);
+malla = new THREE.Mesh(forma, material);
+
+var luzPuntual = new THREE.PointLigth(0xFFFFFF);
+luzPuntual.position.x=10;
+luzPuntual.position.y=10;
+luzPuntual.position.z=10;
+
+escena = new THREE.Scene();
+escena.add(malla);
+escena.add(luzPuntual);
+
+camara= new THREE.PerspectiveCamera();
+camara.position.z=10;
+
+renderer= new THREE.PerspectiveCamera();
+renderer = new THREE.WebGLRender();
+render.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderer.domElemt);
+}
+function loop(){
+requestAnimationFrame(loop);
+malla.rotation.x +=0.01;
+malla.rotation.y +=0.01;
+}
