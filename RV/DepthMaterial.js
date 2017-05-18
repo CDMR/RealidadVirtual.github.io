6c499d6d6e0e34382031e73c07c72a9baa9bb0ba
CarlosdMR <carlosd.marceleno@gmail.com>
2017-05-18 13:28:17 -0500

    Update DepthMaterial.js

--- RV/DepthMaterial.js
+++ RV/DepthMaterial.js
@@ -5,21 +5,17 @@ var malla = new THREE.Mesh(forma,material);
 var luzPuntual = new THREE.PointLight(0xFFFFFF);
 escena = new THREE.Scene();
 escena.add(malla);
-  escena.add(luzPuntual);
-
+escena.add(luzPuntual);
 camara= new THREE.PerspectiveCamera();
 camara.position.z=750;
-
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95, window.innerHeigth*.95);
 document.body.appendChild(renderer.domElement);
 }
 function loop() {
   requestAnimationFrame(loop);
-  
   malla.rotation.x += 0.01;
   malla.rotation.y += 0.01;
-  
   renderer.render(escena, camara);
   }
   var camara, escena, renderer, malla;
