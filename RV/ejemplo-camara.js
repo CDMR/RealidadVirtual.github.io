97195eac0620407eb00a42587184d428303919a2
CarlosdMR <carlosd.marceleno@gmail.com>
2017-05-30 13:49:22 -0500

    Update ejemplo-camara.js

--- RV/ejemplo-camara.js
+++ RV/ejemplo-camara.js
@@ -4,8 +4,8 @@ var planoCercano = 1
 var planoLejano =1000;
 var camara = new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
 camara.position.z=10;
-var cubo= new THREE.Mesh(new THREE.BoxGeometry(2,2,2),new THREE.MeshNormalMaterial() );
-cubo.rotateY(Math.PI/4);
+var cubo= new THREE.Mesh(new THREE.BoxGeometry(8,8,1),new THREE.MeshNormalMaterial() );
+cubo.rotateY(Math.PI/2);
 var esfera1= new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
 esfera1.position.x=30;
 var esfera2= new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
